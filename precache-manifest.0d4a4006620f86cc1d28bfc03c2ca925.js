791c462c2e402e67010a7a714cbbe0c50adcf95a
John Muir <johnmuir@Johns-MacBook-Pro.local>
2020-02-22 11:40:46 +0000

    Updates

---
+++ precache-manifest.0d4a4006620f86cc1d28bfc03c2ca925.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0712ef419807492eda7d4cbf735ac5f",
+    "url": "/card-game-challenge/index.html"
+  },
+  {
+    "revision": "18b22a1593cea79fc9b0",
+    "url": "/card-game-challenge/static/css/main.4e28534e.chunk.css"
+  },
+  {
+    "revision": "68632ef75a8df8bd5b74",
+    "url": "/card-game-challenge/static/js/2.f7e64fa1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/card-game-challenge/static/js/2.f7e64fa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18b22a1593cea79fc9b0",
+    "url": "/card-game-challenge/static/js/main.fb4d9e4e.chunk.js"
+  },
+  {
+    "revision": "0373758f822ccb560c68",
+    "url": "/card-game-challenge/static/js/runtime-main.13214409.js"
+  },
+  {
+    "revision": "903bbc4c448e4f9febb4920f5a15e80a",
+    "url": "/card-game-challenge/static/media/Wario.903bbc4c.jpg"
+  },
+  {
+    "revision": "2a72b74a74258d99a08964d2bc908853",
+    "url": "/card-game-challenge/static/media/babymario.2a72b74a.jpg"
+  },
+  {
+    "revision": "69f44bd2e7c7279b1925d677e673c54f",
+    "url": "/card-game-challenge/static/media/bowser.69f44bd2.jpg"
+  },
+  {
+    "revision": "38c6178d101c72a439f02c9a2121f0aa",
+    "url": "/card-game-challenge/static/media/luigi.38c6178d.jpg"
+  },
+  {
+    "revision": "a48bc99d1e71860ef582a69fdf70389a",
+    "url": "/card-game-challenge/static/media/matt.a48bc99d.jpg"
+  },
+  {
+    "revision": "157a022a579547f3e86780da639c8a02",
+    "url": "/card-game-challenge/static/media/reset.157a022a.png"
+  },
+  {
+    "revision": "9225e89311d3f6d937bb6ad25083e3f7",
+    "url": "/card-game-challenge/static/media/waluigi.9225e893.jpg"
+  },
+  {
+    "revision": "66fafc58fef57b025a75821d5ad4265b",
+    "url": "/card-game-challenge/static/media/yoshi.66fafc58.jpg"
+  }
+]);
